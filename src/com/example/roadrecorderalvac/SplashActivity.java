4192dc914985fdbdfb77496f9a3ec42985cbcaee
Santiago Higuera <shiguera@mercatorlab.com>
2013-12-09 23:31:23 +0100

    Initial commit. Add splashscreen activity

---
+++ src/com/example/roadrecorderalvac/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.roadrecorderalvac;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+	private long splashDelay = 3000; //3 segundos
+
+	  @Override
+	  protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_splash);
+
+	    TimerTask task = new TimerTask() {
+	      @Override
+	      public void run() {
+	        Intent mainIntent = new Intent().setClass(SplashActivity.this, MainActivity.class);
+	        startActivity(mainIntent);
+	        finish();//Destruimos esta activity para prevenit que el usuario retorne aqui presionando el boton Atras.
+	      }
+	    };
+
+	    Timer timer = new Timer();
+	    timer.schedule(task, splashDelay);//Pasado los 6 segundos dispara la tarea
+	  }
+
+}
