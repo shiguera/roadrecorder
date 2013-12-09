bca2fb6ea25a7af2dc7ca671f7a674f45cde4c32
Santiago Higuera <shiguera@mercatorlab.com>
2013-12-10 00:27:31 +0100

    First activity schema running: splash, main, config, help, about

---
+++ src/com/example/roadrecorderalvac/ConfigActivity.java
@@ -0,0 +1,15 @@
+package com.example.roadrecorderalvac;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConfigActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_config);
+	}
+
+
+}
--- src/com/example/roadrecorderalvac/MainActivity.java
+++ src/com/example/roadrecorderalvac/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.roadrecorderalvac;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +20,34 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.menuitem_config:
+			startActivityConfig();
+			break;
+		case R.id.menuitem_help:
+			startActivityHelp();			
+			break;
+		case R.id.menuitem_about:
+			startActivityAbout();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	private void startActivityConfig() {
+		Intent i = new Intent(this, ConfigActivity.class);
+		startActivity(i);
+	}
+	private void startActivityHelp() {
+		Intent i = new Intent(this, HelpActivity.class);
+		startActivity(i);	
+	}
+	private void startActivityAbout() {
+		Intent i = new Intent(this, AboutActivity.class);
+		startActivity(i);
+	}
+	
 
 }
