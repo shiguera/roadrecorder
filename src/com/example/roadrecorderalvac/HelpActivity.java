103c7c41350b634520af5c59e984f1c2b315ad67
Santiago Higuera <shiguera@mercatorlab.com>
2014-01-07 16:37:40 +0100

    Version 1.0 Entregada a Alvac para pruebas

--- src/com/example/roadrecorderalvac/HelpActivity.java
+++ src/com/example/roadrecorderalvac/HelpActivity.java
@@ -1,7 +1,7 @@
 package com.example.roadrecorderalvac;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
